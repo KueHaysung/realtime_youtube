da19795e47ee7fbaceb6014b51f16c911894e08a
Qiu Huicheng <1424438786@qq.com>
2023-09-19 13:41:06 +0800

    fix cron6

--- src/app/api/cron/router.ts
+++ src/app/api/cron/router.ts
@@ -1,24 +1,28 @@
-
 import { db } from "@/lib/db";
 
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  db.set("lastTime", Date.now());
-  const appointmentIds = await db.smembers("incoming_appointment");
-  appointmentIds.map(async (appointmentId) => {
-    const appointment: Appointment[] = await db.smembers(
-      `appointment:${appointmentId}:incoming_appointment`
-    );
-    console.log("appointment", typeof appointment);
-    console.log("appointment[0]", typeof appointment[0]);
-    // const appointmentParsed = JSON.parse(appointment[0]) as Appointment;
-    if (appointment[0].startTime > Date.now()) {
-      await db.srem("incoming_appointment", appointmentId);
-      await db.del(`appointment:${appointmentId}:incoming_appointment`);
-    }
-  });
-  console.log("running a task every minute", Date.now());
-  console.log("任务完成");
-  return NextResponse.json({ ok: true });
+  try {
+    db.set("lastTime", Date.now());
+  } catch (e) {
+    return new Response("cannot set lasttime" + e, { status: 422 });
+  }
+  try {
+    const appointmentIds = await db.smembers("incoming_appointment");
+    appointmentIds.map(async (appointmentId) => {
+      const appointment: Appointment[] = await db.smembers(
+        `appointment:${appointmentId}:incoming_appointment`
+      );
+      // const appointmentParsed = JSON.parse(appointment[0]) as Appointment;
+      if (appointment[0].startTime > Date.now()) {
+        await db.srem("incoming_appointment", appointmentId);
+        await db.del(`appointment:${appointmentId}:incoming_appointment`);
+      }
+      return NextResponse.json({ ok: true });
+    });
+  } catch (e) {
+    return new Response("cannot get appointmentIds" + e, { status: 422 });
+  }
+  
 }
